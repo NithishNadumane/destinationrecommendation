a369705f5ce4489f52aa22b3ec2ace7121fc0129
NithishNadumane <nithishnadumane@gmail.com>
2026-09-24 16:46:15 +0530

    changes

--- backend_v3/app/main.py
+++ backend_v3/app/main.py
@@ -6,10 +6,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-        "http://127.0.0.1:5173"],  # specific origin
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://beliketravellerrecommendation.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(router, prefix="/api")
