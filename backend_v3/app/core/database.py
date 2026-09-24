c2cb08150e54b02ac91297b76ac85b617defe063
NithishNadumane <nithishnadumane@gmail.com>
2026-09-24 17:24:21 +0530

    changes

--- backend_v3/app/core/database.py
+++ backend_v3/app/core/database.py
@@ -1,10 +1,23 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def get_connection():
+
+    database_url = os.getenv("DATABASE_URL")
+
+    # Production / Render / Neon
+    if database_url:
+        return psycopg2.connect(database_url)
+
+    # Local PostgreSQL fallback
     return psycopg2.connect(
         host="localhost",
         port=5432,
         database="beliketraveller",
         user="postgres",
-        password="nithish"
+        password=os.getenv("DB_PASSWORD")
     )
